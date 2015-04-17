02b14e4db9e7764913e0ba50fa2c9c5568345f42
PC20 <PC20@MAESTRIA10-PC>
2015-04-17 12:44:44 -0500

    primero archivos subidos a github.com

---
+++ main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ventana;
+
+/**
+ *
+ * @author PC20
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Primer window=new Primer();
+        window.setTitle("ventana miercoles");
+        window.setVisible(true);
+        
+    }
+    
+}
